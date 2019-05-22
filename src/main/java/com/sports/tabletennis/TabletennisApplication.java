491865bcf81fe7267795e70dc892a49ba6e6d65c
ocassidy <oisin.c@hotmail.co.uk>
2019-05-22 22:47:49 +0100

    intial commit

---
+++ src/main/java/com/sports/tabletennis/TabletennisApplication.java
@@ -0,0 +1,13 @@
+package com.sports.tabletennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletennisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TabletennisApplication.class, args);
+    }
+
+}
